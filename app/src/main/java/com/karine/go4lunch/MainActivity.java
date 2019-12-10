f100985731f42c1faa5274963f6eb04b4c90e239
Karine66 <shon@live.fr>
2019-12-10 22:40:32 +0100

    Multidex problem

--- app/src/main/java/com/karine/go4lunch/MainActivity.java
+++ app/src/main/java/com/karine/go4lunch/MainActivity.java
@@ -1,14 +1,51 @@
 package com.karine.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_activity_coordinator_layout) CoordinatorLayout coordinatorLayout;
+
+
+
+    private static final int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.startSignInActivity();
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+
+    }
+
+    private void startSignInActivity() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Collections.singletonList(new
+                        AuthUI.IdpConfig.GoogleBuilder().build()))
+                .setIsSmartLockEnabled(false, true)
+                .build(),
+                RC_SIGN_IN);
     }
 }
