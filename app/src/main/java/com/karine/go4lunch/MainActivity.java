a4290067fb14dc943b1d594bb54c93a72b0b0bb2
Karine66 <shon@live.fr>
2019-12-12 15:07:06 +0100

    multidex modification

--- app/src/main/java/com/karine/go4lunch/MainActivity.java
+++ app/src/main/java/com/karine/go4lunch/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        MyApplication myApplication = new MyApplication();
     }
 
 
