70e3799d60bd63d15f59c10afda8f0057c527244
Karine66 <shon@live.fr>
2019-12-14 17:11:05 +0100

    sign in with Facebook (Firebase) ok handle response create user ok

--- app/src/main/java/com/karine/go4lunch/MainActivity.java
+++ app/src/main/java/com/karine/go4lunch/MainActivity.java
@@ -14,29 +14,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.AuthUI;;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-
-import java.util.Arrays;
 import java.util.Collections;
 
 import butterknife.BindView;
@@ -98,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode,resultCode,data);
-//        this.handleResponseAfterSignIn(requestCode,resultCode, data);
+       this.handleResponseAfterSignIn(requestCode,resultCode, data);
     }
     @OnClick(R.id.google_btn)
     public void onClickGoogleBtn (View v) {
@@ -109,11 +96,7 @@ public class MainActivity extends AppCompatActivity {
        this.startSignInActivitywithFacebook();
     }
 
-    //Show snack bar with message
-    private void showSnackBar(RelativeLayout mRelativeLayout, String message) {
-        Snackbar.make(mRelativeLayout, message,Snackbar.LENGTH_SHORT).show();
-    }
-
+//Create user in Firebase
     private void startSignInActivitywithGoogle() {
         startActivityForResult(
                 AuthUI.getInstance()
@@ -135,23 +118,21 @@ public class MainActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN);
     }
-//    //method that handles response after signin Activity close
-//    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
-//        IdpResponse response = IdpResponse.fromResultIntent(data);
-//        if (requestCode == RC_SIGN_IN) {
-//            if (resultCode == RESULT_OK) {//sucess
-//                showSnackBar(this.mRelativeLayout, getString(R.string.connection_succeed));
-//            } else { //error
-//                if (response == null) {
-//                    showSnackBar(this.mRelativeLayout, getString(R.string.error_authentication_canceled));
-//                } else if (response.getErrorCodes() == ErrorCodes.NO_NETWORK) {
-//                    showSnackBar(this.mRelativeLayout, getString(R.string.error_no_internet));
-//                } else if (response.getErrorCodes() == ErrorCodes.UNKNOWN_ERROR) {
-//                    showSnackBar(this.mRelativeLayout, getString(R.string.error_unknown_error));
-//                }
-//            }
-//        }
-//    }
-
-
+    //method that handles response after signin Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {//sucess
+                Toast.makeText(this,"Connection succeed", Toast.LENGTH_SHORT).show();
+            } else { //error
+                if (response == null) {
+                    Toast.makeText(this,"Error authentication canceled", Toast.LENGTH_SHORT).show();
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this,"Error no internet", Toast.LENGTH_SHORT).show();
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this,"Error unknown error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 }
