c7a575ae6ccbd4d34bbbe37adba40a022a62c513
Karine66 <shon@live.fr>
2019-12-28 18:58:52 +0100

    getrestaurant with RXJava : in progress

--- app/src/main/java/Utils/Go4LunchService.java
+++ app/src/main/java/Utils/Go4LunchService.java
@@ -10,6 +10,7 @@ public interface Go4LunchService {
 
     String API_KEY = "AIzaSyDRx8LRA_qMswRC9G8DdyY9P-pe-ImYvwg";
 
+    
     //GoogleAPI API Request
     @GET("maps/api/place/nearbysearch/json?key="+API_KEY)
     Observable<GoogleApi> getRestaurants(@Query("location") String location, @Query("radius") int radius, @Query("type") String type);
